81f5a3a9d710448ef9aa794c81c003617f9fe507
ДМ. Па <qtckpuhdsa@gmail.com>
2020-06-30 13:17:38 +0300

    normal hard skills detection

---
+++ wikipedia_api.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 13:49:31 2020
+
+@author: qtckp
+"""
+import io
+import time
+import wikipedia
+from detect_functions import levenshtein_distance_better, levenshtein_distance
+from Ngrams import txt_list_to_grams, print_list
+import numpy as np
+
+
+
+with io.open('my_resume.txt','r', encoding = 'utf-8') as f:
+    doclines = f.readlines()
+    
+ngrams = txt_list_to_grams(doclines,0)
+              
+print_list(ngrams)
+
+
+
+def print_pairs_list(l1, l2):
+    for g, o in zip(l1,l2):
+        print(g)
+        print('-------')
+        print(o)
+        print()
+
+
+# ngrams_eng=[]
+# for g in ngrams:
+#     blob = tb.TextBlob(g)
+#     if len(g)<3:
+#         ngrams_eng.append(g)
+#     elif blob.detect_language() == 'en':
+#         ngrams_eng.append(g)
+#     else:
+#         try:
+#             print(f'translate {g}')
+#             ngrams_eng.append(str(blob.translate()))
+#         except:
+#             print(f"cannot translate {g}")
+#             ngrams_eng.append(g)
+#         time.sleep(2)
+
+
+obj = [wikipedia.search(n) for n in ngrams]
+
+print_pairs_list(ngrams, obj)
+    
+    
+
+
+for g, o in zip(ngrams,obj):
+    if g.lower() in (w.lower() for w in o):
+        print(g.title())
+
+
+
+for g, o in zip(ngrams,obj):
+    
+    dists = [(w,levenshtein_distance_better(g, w)) for w in o]
+    
+    betters = [(w,l) for (w,l) in dists if l<6 and l<len(g)]
+    
+    if len(betters)>0:
+        print(g)
+        print('-------')
+        print(betters)
+        print()
+
+
+
+def clean_list(arr):
+    
+    if len(arr)==0:
+        return arr
+    
+    res = list(set(arr))
+    
+    return res
+    
+
+def get_skills(grams, descriptions):
+    
+    result = []
+    
+    for g, o in zip(grams,descriptions):
+        
+        dists = [(w,levenshtein_distance_better(g, w)) for w in o]
+        
+        betters = [(w,l) for (w,l) in dists if l<6 and l<len(g)]
+        
+        if len(betters)>0:
+            if len(betters) == 1:
+                result.append(betters[0][0])
+            else:
+                #count_of_zero = len((l for _, l in betters if l == 0))
+                
+                words, counts = zip(*betters)
+                counts = np.array(counts, dtype = 'int16')
+                count_of_zero = np.sum(counts == 0)
+                
+                if count_of_zero < 2:
+                    result.append(words[counts.argmin()])
+                else:
+                    counts = np.array((levenshtein_distance_better(g,w) for w in words))
+                    result.append(words[counts.argmin()])           
+    
+    return clean_list(result)
+
+
+r = get_skills(ngrams, obj)
+print_list(r)
